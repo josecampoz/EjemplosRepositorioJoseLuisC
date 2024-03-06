9673fc838bed84562f37806f07544e77cb40b9a6
josecampoz <162385287+josecampoz@users.noreply.github.com>
2024-03-05 19:03:57 -0500

    Create EjerciciosPOO.java

---
+++ EjerciciosPOO/src/main/java/com/mycompany/ejerciciospoo/EjerciciosPOO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejerciciospoo;
+
+/**
+ *
+ * @author Unicomfacauca
+ */
+public class EjerciciosPOO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
